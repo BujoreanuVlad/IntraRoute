ffd58fac81071db024d631d9156edff3114f4510
Kurosaki Kuro <danbuj75@gmail.com>
2021-06-03 09:36:38 +0300

    Moved the functions in main to an unnamed namespace
    Also created an init function and other stuff to draw and use buttons more efficiently

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 const float width {1000};
 const float height {800};
 const size_t NUM_NODES {4};
+const size_t NUM_BUTTONS {2};
 
 const size_t start_node {0};
 const size_t end_node {3};
@@ -17,28 +18,61 @@ const sf::Color background(155, 165, 155);
 std::vector<size_t> my_path;
 
 int m[NUM_NODES][NUM_NODES] = {
-	       {0, 10, 0, 0},
-	       {12, 0, 2, 1},
-	       {0, 3, 0, 7},
-	       {0, 8, 0, 0}
-	     };
+							   {0, 10, 0, 0},
+							   {12, 0, 2, 1},
+							   {0, 3, 0, 7},
+							   {0, 8, 0, 0}
+							  };
 
 structures::Node nodes[NUM_NODES]; 
+structures::Button buttons[NUM_BUTTONS];
 
 namespace {
 
-	void reset(std::vector<size_t> &path) {
+	void init() {
 
+		sf::ContextSettings settings;
+		settings.antialiasingLevel = 8;
+		window = new sf::RenderWindow(sf::VideoMode(width, height), "Intranet path finder", sf::Style::Default, settings);
+		engine::setTimeLink(1);
+
+		if (!font.loadFromFile("Media/Fonts/Hack-Regular.ttf"))
+			window->close();
+
+		for (size_t i {}; i < NUM_NODES; i++) {
+			nodes[i] = structures::newNode(1, i+1);
+			structures::setPosition(nodes[i], 50 + (i/5) * 120, 150 + (i%5) * 100);
+		}
+
+		buttons[0] = structures::newButton(engine::DFS_CODE);
+		setPosition(buttons[0], width / 100, height / 80);
+		setText(buttons[0], "DFS"); 
+		buttons[1] = structures::newButton(engine::BFS_CODE);
+		setPosition(buttons[1], 2 * width / 100 + buttons[0].width, height / 80);
+		setText(buttons[1], "BFS");
+	}
+
+	void reset() {
 		structures::reset(nodes);
-		path.clear();
+		my_path.clear();
 	}
 
 	void choice(int code) {
 
 		switch (code) {
 
-			case engine::DFS_CODE: engine::DFS(*window, my_path, nodes, m, end_node, start_node); break;
-			case engine::BFS_CODE: engine::BFS(*window, my_path, nodes, m, end_node, start_node); break;
+			case engine::DFS_CODE: 
+				reset();
+				engine::DFS(*window, my_path, nodes, m, end_node, start_node);
+				for (auto value : my_path)
+					std::cout << ++value << " ";
+				break;
+			case engine::BFS_CODE:
+				reset();
+				engine::BFS(*window, my_path, nodes, m, end_node, start_node);
+				for (auto value : my_path)
+					std::cout << ++value << " ";
+				break;
 			default: std::cout << "Error, no matching code\n"; break;
 		}
 	}
@@ -48,36 +82,31 @@ namespace {
 		sf::RectangleShape topBar(sf::Vector2f(width, height / 10));
 		topBar.setPosition(0, 0);
 
-		structures::Button DFS_Button = structures::newButton(engine::DFS_CODE);
-		setPosition(DFS_Button, width / 100, height / 80);
-		setText(DFS_Button, "DFS"); 
-		structures::Button BFS_Button = structures::newButton(engine::BFS_CODE);
-		setPosition(BFS_Button, 2 * width / 100 + DFS_Button.width, height / 80);
-		setText(BFS_Button, "BFS");
-
 		window->draw(topBar);
-		draw(*window, DFS_Button);
-		draw(*window, BFS_Button);
+		for (size_t i {}; i < NUM_BUTTONS; i++)
+			draw(*window, buttons[i]);
+	}
+
+	void isButtonClicked(float x, float y) {
+
+		for (size_t i {}; i < NUM_BUTTONS; i++) {
+
+			auto position = buttons[i].rect.getPosition();
+			if ((x >= position.x && x <= position.x + buttons[i].width) &&
+				(y >= position.y && y <= position.y + buttons[i].height)) {
+
+				choice(buttons[i].code);
+			}
+		}
 	}
 }
 
 int main() {
 
-	sf::ContextSettings settings;
-	settings.antialiasingLevel = 8;
-	window = new sf::RenderWindow(sf::VideoMode(width, height), "Intranet path finder", sf::Style::Default, settings);
-	engine::setTimeLink(1);
+	init();
 
-	if (!font.loadFromFile("Media/Fonts/Hack-Regular.ttf"))
-		return 1;
-
-	for (size_t i {}; i < NUM_NODES; i++) {
-		nodes[i] = structures::newNode(1, i+1);
-		structures::setPosition(nodes[i], 50 + (i/5) * 120, 150 + (i%5) * 100);
-	}
-	
 	//Flag that determines wether the algorithm should be run or not
-	bool once {};
+	bool run {};
 	
 	//node that is clicked
 	//And initial coordinates of mouse
@@ -95,7 +124,13 @@ int main() {
 
 			if (event.type == sf::Event::KeyPressed)
 				if (event.key.code == sf::Keyboard::Space)
-					once = true;
+					run = true;
+
+			if (event.type == sf::Event::MouseButtonPressed) {
+			
+				auto position {sf::Mouse::getPosition(*window)};
+				isButtonClicked(position.x, position.y);
+			}
 
 			if (event.type == sf::Event::MouseButtonReleased)
 				   node = nullptr;
@@ -103,14 +138,14 @@ int main() {
 			if (node == nullptr && event.type == sf::Event::MouseButtonPressed) {
 				if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
 
-				   sf::Vector2i position = sf::Mouse::getPosition(*window);
+				   sf::Vector2i position {sf::Mouse::getPosition(*window)};
 	
 				   for (size_t i {}; i < NUM_NODES; i++) {
 
 						if (structures::isInside(nodes[i], position)) {
 
 						 	node = &(nodes[i]);
-							auto node_position = node->rect.getPosition();
+							auto node_position {node->rect.getPosition()};
 							mouse_x_diff = position.x - node_position.x;
 							mouse_y_diff = position.y - node_position.y;
 							break;
@@ -121,7 +156,7 @@ int main() {
 
 			if (node != nullptr) {
 
-				sf::Vector2i position = sf::Mouse::getPosition(*window);
+				sf::Vector2i position {sf::Mouse::getPosition(*window)};
 
 				structures::setPosition(*node, position.x - mouse_x_diff, position.y - mouse_y_diff);
 			}
@@ -131,22 +166,17 @@ int main() {
 
 		draw();
 		structures::draw(*window, nodes, m);
-
-		if (once) {
+/*
+		if (run) {
 
 			reset(my_path);
 
-			//engine::BFS(*window, my_path, nodes, m, 3, 0);
 			choice(engine::BFS_CODE);
 
-			for (auto value : my_path) {
 
-				std::cout << ++value << " ";
-			}
-
-			once = false;
+			run = false;
 		}
-
+*/
 		window->display();
 	}
 
